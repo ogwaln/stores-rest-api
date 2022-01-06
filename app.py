2f50e3731b2d744a8948950d3a1f39786df5f2f4
ogwaln <ogwaln@hotmail.com>
2022-01-06 16:39:37 -0600

    impl create app

--- app.py
+++ app.py
@@ -9,31 +9,37 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-app = Flask(__name__)
 
-app.config['DEBUG'] = True
+def create_app(test_config=None):
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jose'
-api = Api(app)
+    app = Flask(__name__)
 
-jwt = JWT(app, authenticate, identity)  # /auth
+    app.config['DEBUG'] = True
 
-api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Item, '/item/<string:name>')
-api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        'DATABASE_URL', 'sqlite:///data.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.secret_key = 'jose'
+    api = Api(app)
 
-api.add_resource(UserRegister, '/register')
+    jwt = JWT(app, authenticate, identity)  # /auth
 
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
+    api.add_resource(Store, '/store/<string:name>')
+    api.add_resource(Item, '/item/<string:name>')
+    api.add_resource(ItemList, '/items')
+    api.add_resource(StoreList, '/stores')
 
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
+    api.add_resource(UserRegister, '/register')
 
-    app.run(port=5000)
+    # if __name__ == '__main__':
+    # from db import db
+    # db.init_app(app)
+
+    # if app.config['DEBUG']:
+    #     @app.before_first_request
+    #     def create_tables():
+    #         db.create_all()
+
+    # app.run(port=5000)
+
+    return app
--- run.py
+++ run.py
@@ -1,8 +1,6 @@
-from app import app
-from db import db
+from app import create_app
 
-db.init_app(app)
+app = create_app()
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+if __name__ == "__main__":
+    app.run()
